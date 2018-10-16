8f30edd7cf4b487d4464d8ede7406a9fce130fef
jalac9997 <jalac9997@7KYM6Y1.Student.UGDSB.ED>
2018-10-16 09:17:35 -0400

    -created

---
+++ src/unitoneactivityonewaynegretzky/UnitOneActivityOneWayneGretzky.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package unitoneactivityonewaynegretzky;
+
+/**
+ *Jacob Lacey
+ *18 Sept 2018
+ *Stores the String Wayne Gretzky and Displays his Name and Player Number
+ */
+public class UnitOneActivityOneWayneGretzky {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //player number and int is a number without decimal places
+        int playerNumber;
+        //Strings hold a bunch of information (complex)
+        String name;
+        name = "Wayne Gretzky";
+        playerNumber = 99;
+        
+        //print information
+        System.out.println(name+" is number "+playerNumber+".");
+    }
+    
+}
